85f96f366e2f92f6379925157656ad242d9c879a
Tsvetina Rasheva <35625821+TsvetinaRasheva@users.noreply.github.com>
2021-06-11 15:22:39 +0300

    Add files via upload

--- PriceTag.h
+++ PriceTag.h
@@ -3,13 +3,13 @@
 
 template<typename T>
 class PriceTag {
-	double price;
+	T price;
 public:
 	PriceTag() :price(0) {};
 	PriceTag(double _price);
 
-	friend std::ostream& operator<<(std::ostream&, const PriceTag&);
-	friend std::istream& operator>>(std::istream&,  PriceTag&);
+	friend std::ostream& operator<<(std::ostream&, const PriceTag<T>&);
+	friend std::istream& operator>>(std::istream&,  PriceTag<T>&);
 
 	void discount();
 
@@ -26,13 +26,13 @@ template<typename T>
 PriceTag<T>::PriceTag(double _price) : price(_price) {}
 
 template<typename T>
-std::ostream& operator<<(std::ostream& out, const PriceTag<T>& other) {
+std::ostream& operator<<(std::ostream& out, const PriceTag& other) {
 	out << "Price is: " << other.price;
 	return out;
 }
 
 template<typename T>
-std::istream& operator>>(std::istream& in, PriceTag<T>& other) {
+std::istream& operator>>(std::istream& in, PriceTag& other) {
 	std::cout << "Price is: ";
 	double temp;
 	in >> temp;
